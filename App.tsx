d13cd5b7cd89988cb5400805f458f8083bf4ee88
Syahiid Nur Kamil <syahiidkamil.tech@gmail.com>
2024-04-22 01:17:44 +0700

    add bottom tabs navigation

--- App.tsx
+++ App.tsx
@@ -1,43 +1,85 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import {Provider} from 'react-redux';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {CreditCard, Package, Users} from 'lucide-react-native';
+import {AnimatedTabBarNavigator} from 'react-native-animated-nav-tab-bar';
 
 import LoginScreen from './src/screens/Login';
 import store from './src/redux/store';
+import {colors} from './src/constants';
 
 const Stack = createNativeStackNavigator();
+const Tabs = AnimatedTabBarNavigator();
 
-const HomeScreen = () => {
+const TransactionScreen = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.screenContainer}>
+      <Text>Transaction</Text>
     </View>
   );
 };
 
-const DetailsScreen = () => {
+const CustomerScreen = () => {
   return (
-    <View>
-      <Text>Details</Text>
+    <View style={styles.screenContainer}>
+      <Text>Customer</Text>
     </View>
   );
 };
 
-const LaundryAdminStack = () => {
+const ProductScreen = () => {
   return (
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen
-        // navigationKey={isSignedIn ? 'user' : 'guest'}
-        name="Home"
-        component={HomeScreen}
-      />
-      <Stack.Screen
-        // navigationKey={isSignedIn ? 'user' : 'guest'}
-        name="Details"
-        component={DetailsScreen}
-      />
-    </Stack.Navigator>
+    <View style={styles.screenContainer}>
+      <Text>Product</Text>
+    </View>
+  );
+};
+
+const LaundryAdminTabs = () => {
+  return (
+    <Tabs.Navigator
+      initialRouteName="Transaksi"
+      appearance={{
+        floating: true,
+      }}
+      tabBarOptions={{
+        activeTintColor: colors.background,
+        inactiveTintColor: colors.primary,
+        activeBackgroundColor: colors.primary,
+      }}
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, size}: {focused: boolean; size: number}) => {
+          let icon;
+          if (route.name === 'Transaksi') {
+            icon = (
+              <CreditCard
+                color={focused ? colors.background : colors.primary}
+                size={size}
+              />
+            );
+          } else if (route.name === 'Pelanggan') {
+            icon = (
+              <Users
+                color={focused ? colors.background : colors.primary}
+                size={size}
+              />
+            );
+          } else if (route.name === 'Produk') {
+            icon = (
+              <Package
+                color={focused ? colors.background : colors.primary}
+                size={size}
+              />
+            );
+          }
+          return icon;
+        },
+      })}>
+      <Tabs.Screen name="Transaksi" component={TransactionScreen} />
+      <Tabs.Screen name="Pelanggan" component={CustomerScreen} />
+      <Tabs.Screen name="Produk" component={ProductScreen} />
+    </Tabs.Navigator>
   );
 };
 
@@ -57,8 +99,17 @@ export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <AuthStack />
+        <LaundryAdminTabs />
       </NavigationContainer>
     </Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.background,
+  },
+});
